501fc993ec945d454a161f552ea6ab0a71b094c9
Tonomi <163392234+hwanginseop@users.noreply.github.com>
2025-06-03 16:42:42 +0900

    [Bronze IV] Title: 숫자의 합, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/11720. 숫자의 합/숫자의 합.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int n;
+string str;
+int sum = 0;
+
+int main()
+{
+	cin >> n >> str;
+	for (int i = 0; i < n; i++)
+	{
+		sum += str[i] - '0';
+	}
+	cout << sum;
+	return 0;
+}
